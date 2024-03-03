8d0fbab0f8ffa9fad3c5f0863ce440966cb5c07b
nmaketh <n.riak@alustudent.com>
2024-03-03 19:27:38 +0000

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,33 +1,18 @@
 #!/usr/bin/python3
-"""Return the number of subscribers of a given subreddit"""
-
-
-import requests
+# get subs
+from requests import get
+from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    """function that fetches number_of_subscribers"""
-    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
-
+    """subs"""
+    head = {'User-Agent': 'Dan Kazam'}
+    count = get('https://www.reddit.com/r/{}/about.json'.format(
+        subreddit), headers=head).json()
     try:
-        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
-        return RESPONSE.json().get("data").get("subscribers")
+        return count.get('data').get('subscribers')
+    except:
+        return 0
 
-    except Exception:
-        return 0sh: 1: Return the number of subscribers of a given subreddit: not found
-"""Script that fetch 10 hot post for a given subreddit."""
-import requests
-
-
-def number_of_subscribers(subreddit):
-    """Return the number of subscribers for the given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json().get('data')
-        if data:
-            return data.get('subscribers', 0)
-    return 0
+if __name__ == "__main__":
+    number_of_subscribers(argv[1])
