00ae5fe628e9c9a20dde9f841f4afa03f8b6b0a9
nmaketh <n.riak@alustudent.com>
2024-03-03 18:06:03 +0000

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
+"""Return the number of subscribers of a given subreddit"""
+
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """function that fetches number_of_subscribers"""
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+
+    try:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        return RESPONSE.json().get("data").get("subscribers")
+
+    except Exception:
+        return 0sh: 1: Return the number of subscribers of a given subreddit: not found
 """Script that fetch 10 hot post for a given subreddit."""
 import requests
 
